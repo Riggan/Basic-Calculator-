261ba1aab7540cdded651deab816ce75c97a6169
Aniket <aniketdogra8@gmail.com>
2018-05-05 00:39:45 +0530

    First Commit

---
+++ WebApplication2/CalcOperation/CalcOperation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.CalcOperation
+{
+    public class CalcOperation
+    {
+        public  Total(int value1, int value2, string calc)
+        {
+            int total = 0;
+            switch (calc)
+            {
+                case "+":
+                    total = value1 + value2;
+                    break;
+                case "-":
+                    total = value1 - value2;
+                    break;
+                case "*":
+                    total = value1 * value2;
+                    break;
+                case "/":
+                    total = value1 / value2;
+                    break;
+                default:
+                    total = 0;
+                    break;
+            }
+            return Content(total);
+        }
+    }
+}
---
+++ WebApplication2/Controllers/CalcController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CalcOperation; 
+
+namespace WebApplication2.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Total(int value1, int value2, string calc);
+        
+        }
+    }
+}
